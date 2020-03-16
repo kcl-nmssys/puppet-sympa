811cd91c19a0f184503d485d28c1d97b8b42e28d
Xand Meaden <xand@xand.uk>
2020-03-16 17:24:37 +0000

    fix copy/paste fail

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,15 +5,15 @@ class sympa::config {
       enable => true;
   }
 
-  $listmasters_txt             = join($exim::listmasters, ',')
-  $supported_lang_txt          = join($exim::supported_lang, ',')
-  $remove_headers_txt          = join($exim::remove_headers, ',')
-  $rfc2369_header_fields_txt   = join($exim::rfc2369_header_fields, ',')
-  $use_blacklist_txt           = join($exim::use_blacklist, ',')
-  $owner_domain_txt            = join($exim::owner_domain, ' ')
-  $parsed_family_files_txt     = join($exim::parsed_family_files, ',')
-  $dkim_add_signature_to_txt   = join($exim::dkim_add_signature_to, ',')
-  $dkim_signature_apply_on_txt = join($exim::dkim_signature_apply_on, ',')
+  $listmasters_txt             = join($sympa::listmasters, ',')
+  $supported_lang_txt          = join($sympa::supported_lang, ',')
+  $remove_headers_txt          = join($sympa::remove_headers, ',')
+  $rfc2369_header_fields_txt   = join($sympa::rfc2369_header_fields, ',')
+  $use_blacklist_txt           = join($sympa::use_blacklist, ',')
+  $owner_domain_txt            = join($sympa::owner_domain, ' ')
+  $parsed_family_files_txt     = join($sympa::parsed_family_files, ',')
+  $dkim_add_signature_to_txt   = join($sympa::dkim_add_signature_to, ',')
+  $dkim_signature_apply_on_txt = join($sympa::dkim_signature_apply_on, ',')
 
   file {
     [$sympa::etc, $sympa::home]:
